065c333ac32734b5ca097a2f758e232b82528fe3
Ben Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2021-11-14 03:30:35 -0800

    Finalize exports for publish (#8)
    * adding yargs for cli utilities, adding logic for --file/-f flag for explicitly passing lock file path, adding logic for auto-detecting lock location based on cwd
    
    * removing "deps" property in DepFiles type - not doing any analyzing of package.json yet
    
    * adding .gitignore to exclude dev-only files
    
    * adding banner to cli build in rollup for node shebang

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,66 @@
+import path from "path";
+import fs from "fs";
 import startIndeps from "./api";
+import { LockType } from "./api/parsers";
+import logger from "./api/logger";
 
 // @FIX add logic for automatically detecting lock/package.json
 // and for arg overrides
-startIndeps({
-  lock: {
-    type: "yarn",
-    path: "/Users/benkincaid/Projects/npm-depviz/yarn.lock"
-  },
-  deps: {
-    path: "/Users/benkincaid/Projects/npm-depviz/package.json"
+
+const argv = require("yargs")
+  .scriptName("indeps")
+  .usage("Usage: $0 [options]")
+  .help("h")
+  .alias("h", "help")
+  .alias("f", "file")
+  .nargs("f", 1)
+  .describe(
+    "f",
+    "The yarn.lock to use for the visualization. Defaults to yarn.lock in current directory."
+  ).argv;
+
+const checkIfExists = (filePath: string): boolean => {
+  const exists = fs.existsSync(filePath);
+
+  if (!exists) {
+    logger.log({
+      level: "critical",
+      msg: `No file found at: ${filePath}`
+    });
+    process.exit(1);
   }
+
+  return true;
+};
+
+const getLockInfo = (): { path: string; type: LockType } => {
+  // handle if --f was passed
+  if (argv.f) {
+    if (argv.f[0] === "/") {
+      checkIfExists(argv.f);
+      return { path: argv.f, type: "yarn" };
+    }
+    const relativePath = path.join(process.cwd(), argv.f);
+    checkIfExists(relativePath);
+
+    return {
+      path: relativePath,
+      type: "yarn"
+    };
+  }
+
+  // handle auto-detection
+  const autoPath = path.join(process.cwd(), "./yarn.lock");
+  checkIfExists(autoPath);
+
+  return {
+    path: autoPath,
+    type: "yarn"
+  };
+};
+
+const lock = getLockInfo();
+
+startIndeps({
+  lock: lock
 });
