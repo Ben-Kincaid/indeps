ada95cb4d0dd07622d5c32c0a96bf72a5a2c80b6
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2021-11-13 21:59:52 -0800

    moving client outside of src

---
+++ client/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Viewer from "./Viewer";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Viewer />, document.getElementById("app"));
+});
