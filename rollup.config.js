fce96b5ebf8b04697cbd931d2835bc89f9b48181
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2021-11-13 22:10:21 -0800

    adding rolup config for bundling node files

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+const env = process.env.PKG_ENV || "dev";
+
+export default [
+  {
+    input: `src/index.ts`,
+    output: {
+      dir: "lib",
+      format: "cjs",
+      sourcemap: env !== "prod",
+      exports: "default",
+      name: "main.ts"
+    },
+    plugins: [typescript(), commonjs(), nodeResolve()]
+  },
+  {
+    input: `src/cli.ts`,
+    output: {
+      dir: "lib",
+      format: "cjs",
+      sourcemap: env !== "prod",
+      exports: "none",
+      name: "main.ts"
+    },
+    plugins: [typescript(), commonjs(), nodeResolve()]
+  }
+];
