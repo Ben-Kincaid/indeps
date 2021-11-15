b4af63ef6247b2dc8d55e59932e21b0f15a838ae
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2021-11-14 18:29:10 -0800

    adding markup for version/package name in navbar

--- client/components/NavBar/NavBar.tsx
+++ client/components/NavBar/NavBar.tsx
@@ -1,13 +1,17 @@
 import React, { ReactElement } from "react";
 import styles from "./NavBar.module.scss";
+import logoLight from "../../assets/logo-light.png";
 
 function NavBar(): ReactElement {
   return (
     <nav className={styles.nav}>
       <div className={styles.branding}>
-        <h4 className={styles.brandingHeader}>Indeps</h4>
+        <img className={styles.brandImage} src={logoLight} alt="Indeps" />
+        <span className={styles.brandVersion}>v0.6.0</span>
       </div>
-      <div className={styles.navigation}></div>
+      <div className={styles.name}>
+        <h5>project-name!</h5>
+      </div>
     </nav>
   );
 }
