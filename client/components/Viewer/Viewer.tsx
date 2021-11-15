1e049f068ba7e97680c33ba033a09740064dd309
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2021-11-14 18:29:36 -0800

    changing destructuring of useData after change of return structure

--- client/components/Viewer/Viewer.tsx
+++ client/components/Viewer/Viewer.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
-import useData from "../../hooks/useData";
+import React from "react";
+import useData from "../../hooks/useData/useData";
 import styles from "./Viewer.module.scss";
 
 const Viewer = () => {
-  const data = useData();
+  const { lockData } = useData();
 
   return (
-    <pre className={styles.viewerPre}>{JSON.stringify(data, null, "\t")}</pre>
+    <pre className={styles.viewerPre}>
+      {JSON.stringify(lockData, null, "\t")}
+    </pre>
   );
 };
 
