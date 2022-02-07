3b81899ecc032c88957beeaa083dc48252061b00
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2022-02-06 17:44:43 -0800

    add utils dir in api tests, add getFixture utility for reading from fixture files

---
+++ src/api/__tests__/utils/getFixture.ts
@@ -0,0 +1,19 @@
+import fs from "fs";
+import path from "path";
+
+/**
+ * Get raw data from an available fixture file
+ */
+function getFixture<T = string>(
+  fileName: string,
+  parseJson: boolean = false
+): T {
+  const data = fs.readFileSync(
+    path.resolve(__dirname, `../fixtures/${fileName}`),
+    "utf8"
+  );
+
+  return parseJson ? JSON.parse(data) : data;
+}
+
+export default getFixture;
