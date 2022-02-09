5f150bc9bebe3acca56c2f83bb6c54d04877d0d0
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2022-02-08 22:02:49 -0800

    change fixture source for computePackageTags tests

--- src/api/__tests__/computePackageTags.test.ts
+++ src/api/__tests__/computePackageTags.test.ts
@@ -53,19 +53,17 @@ describe("computePackageTags", () => {
     expect(tags).toEqual(["TAG_DEV_DEPENDENCY"]);
   });
 
-  it("creates proper package tags for project + dev dependency (similiar spec)", () => {
+  it("creates proper package tags for nested dependency", () => {
     const dependency: LockDependency = {
-      name: "path-to-regexp",
-      version: "6.2.0",
-      resolved:
-        "https://registry.npmjs.org/path-to-regexp/-/path-to-regexp-6.2.0.tgz",
-      integrity:
-        "sha512-f66KywYG6+43afgE/8j/GoiNyygk/bnoCbps++3ErRKsIYkGGupyv07R2Ok5m9i67Iqc+T2g1eAUGUPzWhYTyg==",
-      specifications: ["^6.2.0"]
+      name: "isarray",
+      version: "0.0.1",
+      resolved: "https://registry.npmjs.org/isarray/-/isarray-0.0.1.tgz",
+      integrity: "sha1-ihis/Kmo9Bd+Cav8YDiTmwXR7t8=",
+      specifications: ["0.0.1"]
     };
 
     const parsedPkg = getFixture<PackageJson>(
-      "package--dev-and-proj-similar.mock.json",
+      "package--shared.mock.json",
       true
     );
 
@@ -75,124 +73,6 @@ describe("computePackageTags", () => {
       lockDependency: dependency
     });
 
-    console.log(parsedPkg.devDependencies);
-
-    expect(tags).toEqual(["TAG_DEV_DEPENDENCY", "TAG_DEPENDENCY"]);
-  });
-
-  it("creates proper package tags for project + dev dependency (different spec, same resolution)", () => {
-    const dependency = {
-      name: "path-to-regexp",
-      version: "6.2.0",
-      resolved:
-        "https://registry.npmjs.org/path-to-regexp/-/path-to-regexp-6.2.0.tgz",
-      integrity:
-        "sha512-f66KywYG6+43afgE/8j/GoiNyygk/bnoCbps++3ErRKsIYkGGupyv07R2Ok5m9i67Iqc+T2g1eAUGUPzWhYTyg==",
-      specifications: ["^6.2.0", "^6.2.0"]
-    };
-
-    const parsedPkg = getFixture<PackageJson>(
-      "package--dev-and-proj-different.mock.json",
-      true
-    );
-
-    const tags = computePackageTags({
-      dependencies: parsedPkg.dependencies,
-      devDependencies: parsedPkg.devDependencies,
-      lockDependency: dependency
-    });
-
-    expect(tags).toEqual(["TAG_DEV_DEPENDENCY", "TAG_DEPENDENCY"]);
+    expect(new Set(tags)).toEqual(new Set(["TAG_SUB_DEPENDENCY"]));
   });
 });
-
-// import { createDependencyData, createDependencyGraph } from "src/api";
-// import loggerInstance from "src/api/logger";
-// import { ParsedLock } from "src/api/parsers";
-// import npmParser from "src/api/parsers/npm";
-// import yarnParser from "src/api/parsers/yarn";
-// import { getFixture } from "src/api/__tests__/utils";
-
-// function createParsedLock(
-//   fixtureName: string,
-//   type: "npm" | "yarn" = "npm",
-//   pkg: PackageJson | undefined
-// ) {
-//   const data = getFixture(fixtureName, false);
-//   if (type === "npm") {
-//     return npmParser(data, pkg!);
-//   } else {
-//     return yarnParser(data);
-//   }
-// }
-
-// describe("createDependencyData", () => {
-//   it("creates an array of proper full dependency objects, simple", () => {
-//     const parsedPkg = getFixture<PackageJson>(
-//       "package--single.mock.json",
-//       true
-//     );
-
-//     const parsedLock = createParsedLock(
-//       "package-lock--single.mock.json",
-//       "npm",
-//       parsedPkg
-//     );
-
-//     const parsedGraph = createDependencyGraph(parsedLock);
-
-//     const parsed = createDependencyData({
-//       lock: parsedLock,
-//       pkg: parsedPkg,
-//       graph: parsedGraph
-//     });
-
-//     expect(parsed).toEqual([
-//       {
-//         name: "path-to-regexp",
-//         version: "6.2.0",
-//         resolved:
-//           "https://registry.npmjs.org/path-to-regexp/-/path-to-regexp-6.2.0.tgz",
-//         integrity:
-//           "sha512-f66KywYG6+43afgE/8j/GoiNyygk/bnoCbps++3ErRKsIYkGGupyv07R2Ok5m9i67Iqc+T2g1eAUGUPzWhYTyg==",
-//         specifications: ["^6.2.0"],
-//         tags: ["TAG_DEPENDENCY"],
-//         paths: [["path-to-regexp@6.2.0"]]
-//       }
-//     ]);
-//   });
-//   it("creates an array of proper full dependency objects, linked", () => {
-//     const parsedPkg = getFixture<PackageJson>(
-//       "package--linked.mock.json",
-//       true
-//     );
-
-//     const parsedLock = createParsedLock(
-//       "package-lock--linked.mock.json",
-//       "npm",
-//       parsedPkg
-//     );
-
-//     const parsedGraph = createDependencyGraph(parsedLock);
-
-//     const parsed = createDependencyData({
-//       lock: parsedLock,
-//       pkg: parsedPkg,
-//       graph: parsedGraph
-//     });
-
-//     expect(parsed).toEqual([
-//       {
-//         name: "path-to-regexp",
-//         version: "6.2.0",
-//         resolved:
-//           "https://registry.npmjs.org/path-to-regexp/-/path-to-regexp-6.2.0.tgz",
-//         integrity:
-//           "sha512-f66KywYG6+43afgE/8j/GoiNyygk/bnoCbps++3ErRKsIYkGGupyv07R2Ok5m9i67Iqc+T2g1eAUGUPzWhYTyg==",
-//         specifications: ["^6.2.0"],
-//         tags: ["TAG_DEPENDENCY"],
-//         paths: [["path-to-regexp@6.2.0"]]
-//       }
-//     ]);
-//   });
-// });
