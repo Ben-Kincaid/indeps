e5d8b6c1f4629c0c24519bab9d7cb83da8269552
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2022-02-14 22:01:29 -0800

    add tsdoc to new utils

--- src/utils/fileExists.ts
+++ src/utils/fileExists.ts
@@ -1,5 +1,12 @@
 import fs from "fs";
 
+/**
+ * Simple method for checking the existence of a file.
+ *
+ * @param filePath - A string representation of the absolute path to a file.
+ *
+ * @returns True if file exists, false if not.
+ */
 export default function fileExist(filePath: string): boolean {
   return fs.existsSync(filePath);
 }
--- src/utils/getLockTypeFromPath.ts
+++ src/utils/getLockTypeFromPath.ts
@@ -1,5 +1,17 @@
 import { LockType } from "src/parsers/types";
 
+/**
+ * Parse the requested lock file type by its path.
+ *
+ * @remarks
+ * A naive method that takes a file path and return the `LockType` by analyzing the file name & extension.
+ *
+ * @param path - A string representing the path to the lockfile.
+ *
+ * @returns A `LockType` (either "npm" or "yarn"), or null if unable to be detected.
+ *
+ * @internal
+ */
 export default function getLockTypeFromPath(
   path: string
 ): LockType | null {
