a297ae964c2c98e73cd7db0b17e6a8dd5bd03dc9
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2022-02-21 21:30:51 -0800

    add logic for sorting by subset in sortSpecifications if min version is matching, add test case

--- src/__tests__/sortSpecifications.test.ts
+++ src/__tests__/sortSpecifications.test.ts
@@ -11,4 +11,23 @@ describe("sortSpecifications", () => {
       "1.0.0"
     ]);
   });
+  it("correctly sorts ranges with same minimum version (subset ordering)", () => {
+    const specs = [
+      "^4.0.0 < 4.1.0",
+      "4.0.0",
+      "^4.0.0",
+      "^4.0.0 <= 4.1.0",
+      "^4.0.0 < 4.2.0",
+      "^4.0.0 < 4.3.0"
+    ];
+
+    expect(sortSpecifications(specs)).toEqual([
+      "^4.0.0",
+      "^4.0.0 < 4.3.0",
+      "^4.0.0 < 4.2.0",
+      "^4.0.0 <= 4.1.0",
+      "^4.0.0 < 4.1.0",
+      "4.0.0"
+    ]);
+  });
 });
--- src/parsers/utilities/sortSpecifications.ts
+++ src/parsers/utilities/sortSpecifications.ts
@@ -1,6 +1,6 @@
 import semverLt from "semver/functions/lt";
 import semverMinVersion from "semver/ranges/min-version";
-
+import semverSubset from "semver/ranges/subset";
 /**
  * Sort an array of package semver specifications
  *
@@ -13,14 +13,25 @@ function sortSpecifications(
   specifications: Array<string>
 ): Array<string> {
   return specifications.sort((a, b) => {
+    // get minimum possible versions for ranges
     const aMin = semverMinVersion(a);
     const bMin = semverMinVersion(b);
 
-    if (!aMin || !bMin || aMin.raw === bMin.raw) {
-      return 0;
+    // if there is no valid range returned for either, return 0
+    if (!aMin || !bMin) return 0;
+
+    // if both min versions are the same, place subsets ahead
+    if (aMin.raw === bMin.raw) {
+      if (semverSubset(a, b)) {
+        return 1;
+      } else {
+        return -1;
+      }
     } else if (semverLt(aMin.raw, bMin.raw)) {
+      // otherwise, push a ahead if less than b min version
       return 1;
     }
+
     return -1;
   });
 }
