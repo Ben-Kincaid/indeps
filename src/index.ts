4cb821f3b1c78f104fba32d8185d2c9246f9cf5b
Ben-Kincaid <30325728+Ben-Kincaid@users.noreply.github.com>
2022-03-27 21:20:11 -0700

    export parseLock, parsePkg, createDpeendencyGraph, createDependencyData, createViewer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import { LockType } from "src/parsers/types";
+import { parseLock, parsePkg, LockType } from "src/parsers";
+import { createViewer } from "src/viewer";
+import createDependencyGraph from "src/utils/createDependencyGraph";
+import createDependencyData from "src/utils/createDependencyData";
 import { StartOpts, LockInfo, PkgInfo } from "src/types";
 
-export { StartOpts, LockInfo, PkgInfo, LockType };
+export {
+  parseLock,
+  parsePkg,
+  createDependencyGraph,
+  createDependencyData,
+  createViewer,
+  StartOpts,
+  LockInfo,
+  PkgInfo,
+  LockType
+};
